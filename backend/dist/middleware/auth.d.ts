b8d7d69ade89dbc0e873519a4940930050edbc9c
Henok-Al <henok.gebresenbet@gmail.com>
2026-01-17 14:37:44 +0300

    Update gitignore to exclude build files and add proper ignores

--- backend/dist/middleware/auth.d.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-import { IUser } from '../models/User';
-interface AuthRequest extends Request {
-    user?: IUser;
-}
-declare const auth: (req: AuthRequest, res: Response, next: NextFunction) => Promise<Response<any, Record<string, any>> | undefined>;
-export default auth;
-//# sourceMappingURL=auth.d.ts.map
